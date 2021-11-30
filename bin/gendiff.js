240d6cadad8ce01e9f66c4d6c90e9557cf15ddbc
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2021-11-30 11:14:37 +0500

    update: create index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,23 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import gendiff from '../src/index.js';
+
 const program = new Command();
 
 program
   .arguments('<filepath1> <filepath2>')
+  .name("gendiff")
   .version('0.0.1', '-V, --vers', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
-  .parse()
+  .option('-f, --format [type]', 'output format')
   .help('-h, --help', 'output usage information')
-  .options('-f, --format [type]', 'output format');
+  .action(gendiff(arguments));
+
+program.parse();
 
 const options = program.opts();
 console.log('Options:');
 if (options.version) console.log(program.version);
 if (options.help) console.log(`$shell help`);
+if (options.format) console.log(program.option);
