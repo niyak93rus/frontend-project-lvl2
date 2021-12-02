b4bc7af04586059c68e6912c705d6e907602dbc2
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2021-12-02 12:19:51 +0500

    update: change genDiff function

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 import { readFileSync } from 'fs';
+import _ from 'lodash'
 
 const genDiff = (filepath1, filepath2) => {
-  /* Требования: 
-    Сравниваются данные, а не строки файлов.
-    Две строчки дифа, отвечающие за различия поля, должны находиться рядом.
-    Причём вначале должна выводиться строка относящаяся к первому файлу, а затем строка относящаяся ко второму файлу.
-    Результатом работы функции genDiff() является строка. */
-
-  // прочитать файл1
-  // прочитать файл2
-  // сравниваем ключи
-  // если ключа нет, выводим ключ-занчение с минусом
-  // если ключ есть - 
-    // сравниваем занчения
-    // если все совпало, просто выводим строку ключ-значение
-    // если нет - выводим ключ-значение из первого файла, ниже - из второго
   const file1 = readFileSync(filepath1);
   const file2 = readFileSync(filepath2);
   
   const obj1 = JSON.parse(file1);
   const obj2 = JSON.parse(file2);
 
-  console.log(obj1, obj2);
-  
-  const result = '';
+  const keys1 = _.keys(obj1);
+  const keys2 = _.keys(obj2);
+  const keys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(keys);
 
-  return result;
+  let result = '{\n';
+  sortedKeys.map((key) => {
+      if (!_.has(obj2, key)) {
+        result += `  - ${key}: ${obj1[key]} \n`;
+      } else if (!_.has(obj1, key)) {
+        result += `  + ${key}: ${obj2[key]} \n`;
+      } else if (obj1[key] !== obj2[key]) {
+        result += `  - ${key}: ${obj1[key]} \n`;
+        result += `  + ${key}: ${obj2[key]} \n`;
+      } else {
+        result += `    ${key}: ${obj1[key]} \n`;
+      }
+  });
+
+  result += '}'
+
+  return console.log(result.trim());
 };
 
 export default genDiff;
