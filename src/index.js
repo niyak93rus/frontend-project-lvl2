3ecac819615feaf13fe619162eaa072ad9b2ba89
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2021-12-18 09:54:45 +0500

    work on building tree

--- src/index.js
+++ src/index.js
@@ -1,9 +1,52 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-param-reassign */
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
 import { readFileSync } from 'fs';
 import _ from 'lodash';
 import * as path from 'path';
 import parse from './parsers.js';
+import stylish from './stylish.js';
+
+const getDifferencesOfObjects = (tree1, tree2) => {
+  const keys1 = _.keys(tree1);
+  const keys2 = _.keys(tree2);
+  const keys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(keys);
+  console.log(`KEYS: ${sortedKeys}`);
+
+  const propertiesCollection = sortedKeys.reduce((result, key) => {
+    const newObj = {};
+    console.log(`CURRENT KEY: ${key}`);
+    if (_.isObject(tree1[key]) && _.isObject(tree2[key])) {
+      newObj.property = key;
+      result.push(newObj, getDifferencesOfObjects(tree1[key], tree2[key]));
+    }
+    if (!_.isObject(tree1[key]) || !_.isObject(tree2[key])) {
+      newObj.property = key;
+      if (!_.has(tree2, key)) {
+        newObj.status = 'deleted';
+        newObj.value = tree1[key];
+      } else if (!_.has(tree1, key)) {
+        newObj.status = 'added';
+        newObj.value = tree2[key];
+      } else if (_.has(tree1, key) && _.has(tree2, key) && tree1[key] !== tree2[key]) {
+        newObj.status = 'changed';
+        newObj.oldValue = tree1[key];
+        newObj.newValue = tree2[key];
+      } else {
+        newObj.status = 'unchanged';
+        newObj.value = tree1[key];
+      }
+      result.push(newObj);
+    }
+    console.log(newObj);
+
+    return result;
+  }, []);
+
+  return propertiesCollection;
+};
 
 const genDiff = (filename1, filename2) => {
   const path1 = path.resolve(process.cwd(), filename1);
@@ -18,29 +61,28 @@ const genDiff = (filename1, filename2) => {
   const obj1 = parse(file1, format1);
   const obj2 = parse(file2, format2);
 
-  const keys1 = _.keys(obj1);
-  const keys2 = _.keys(obj2);
-  const keys = _.union(keys1, keys2);
-  const sortedKeys = _.sortBy(keys);
+  const unformattedTree = getDifferencesOfObjects(obj1, obj2);
 
-  let result = '{\n';
-  sortedKeys.map((key) => {
-    if (!_.has(obj2, key)) {
-      result += `  - ${key}: ${obj1[key]}\n`;
-    } else if (!_.has(obj1, key)) {
-      result += `  + ${key}: ${obj2[key]}\n`;
-    } else if (obj1[key] !== obj2[key]) {
-      result += `  - ${key}: ${obj1[key]}\n`;
-      result += `  + ${key}: ${obj2[key]}\n`;
-    } else {
-      result += `    ${key}: ${obj1[key]}\n`;
-    }
-    return result;
-  });
+  // FORMAT TREE
+  const buildAST = (arr) => {
+    const resultObj = {};
+    arr.flatMap((item) => {
+      if (item.status === 'unchanged') {
+        resultObj.value = item.value;
+      } else if (item.status === 'added') {
+        // TODO
+      } else if (item.status === 'deleted') {
+        // TODO
+      } else {
+        // TODO
+      }
+      return null;
+    });
+  };
 
-  result += '}';
+  const result = buildAST(unformattedTree);
 
-  return result.trim();
+  return unformattedTree;
 };
 
 export default genDiff;
---
+++ src/stylish.js
@@ -0,0 +1,28 @@
+const formatter = (obj, replacer = ' ', spacesCount = 1) => {
+  let depthLevel = 0;
+  const inner = (data) => {
+    if (typeof data !== 'object' || data === null) {
+      return String(data);
+    }
+    const entries = Object.entries(data);
+    depthLevel += 1;
+    const result = entries
+      .reduce((acc, [key, value], currentIndex) => {
+        const str = `${replacer.repeat(spacesCount * depthLevel)}${key}: ${inner(value)}`;
+        acc.push(str);
+        if (currentIndex === entries.length - 1) {
+          while (depthLevel >= 1) {
+            acc.push(`${replacer.repeat(spacesCount * (depthLevel - 1))}}`);
+            depthLevel -= 1;
+          }
+        }
+
+        return acc;
+      }, ['{']);
+    return result.join('\n');
+  };
+
+  return inner(obj);
+};
+
+export default formatter;
