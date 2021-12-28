371b950242e2de5c7104a78ba68931186ecbd615
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2021-12-28 10:59:10 +0500

    fix bin file #2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
-import { program } from 'commander';
+import { Command, Option } from 'commander';
 import genDiff from '../formatters/index.js';
 
-const options = program.opts();
+const gendiff = () => {
+  const program = new Command();
+  const options = program.opts();
+  const formatOption = new Option('-f, --format [type]', 'output format')
+    .default('stylish')
+    .choices(['stylish', 'plain', 'json']);
 
-if (options.version) console.log(program.version);
-if (options.help) console.log('$shell help');
+  program
+    .version('0.0.1')
+    .arguments('<filepath1> <filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .helpOption('-h, --help', 'output usage information')
+    .addOption(formatOption)
+    .action((path1, path2) => console.log(genDiff(path1, path2, options.format)));
 
-program
-  .arguments('<filepath1> <filepath2>')
-  .version('0.0.1', '-V, --vers', 'output the current version')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --formatter [type]', 'stylish', 'stylish')
-  .action((path1, path2) => console.log(genDiff(path1, path2, options.format)));
+  program.parse(process.argv);
+};
 
-program.parse(process.argv);
+gendiff();
