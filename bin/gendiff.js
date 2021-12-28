2fd0a02c4b540db9bcd632fb5ad4b0f67a1ae6cc
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2021-12-28 10:55:52 +0500

    fix bin file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,17 +4,16 @@
 import { program } from 'commander';
 import genDiff from '../formatters/index.js';
 
+const options = program.opts();
+
+if (options.version) console.log(program.version);
+if (options.help) console.log('$shell help');
+
 program
   .arguments('<filepath1> <filepath2>')
   .version('0.0.1', '-V, --vers', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --formatter [type]', 'stylish', 'stylish')
-  .action(genDiff);
+  .action((path1, path2) => console.log(genDiff(path1, path2, options.format)));
 
 program.parse(process.argv);
-
-const options = program.opts();
-
-if (options.version) console.log(program.version);
-if (options.help) console.log('$shell help');
-console.log(program.action);
