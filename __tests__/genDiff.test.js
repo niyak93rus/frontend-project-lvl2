1a0e9d98b226f3120aa631df412c0465ee19098c
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2022-01-05 11:09:53 +0500

    fix tests: use test.each

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
@@ -10,34 +10,13 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('generate STYLISH diff between two JSON files', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-  const formatName = 'stylish';
-  const expectedFile = fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8');
-  expect(genDiff(file1, file2, formatName)).toBe(expectedFile);
-});
-
-test('generate STYLISH diff between two YAML files', () => {
-  const file1 = getFixturePath('file1.yml');
-  const file2 = getFixturePath('file2.yml');
-  const formatName = 'stylish';
-  const expectedFile = fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8');
-  expect(genDiff(file1, file2, formatName)).toBe(expectedFile);
-});
-
-test('generate PLAIN diff between JSON and YAML files', async () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.yml');
-  const formatName = 'plain';
-  const expectedFile = fs.readFileSync(getFixturePath('resultPlain.txt'), 'utf-8');
-  expect(genDiff(file1, file2, formatName)).toBe(expectedFile);
-});
-
-test('generate JSON format diff between JSON and YAML files', async () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-  const formatName = 'json';
-  const expectedFile = fs.readFileSync(getFixturePath('resultJSON.txt'), 'utf-8');
-  expect(genDiff(file1, file2, formatName)).toBe(expectedFile);
+describe('test diff generation between two files', () => {
+  test.each([
+    ['stylish', getFixturePath('file1.json'), getFixturePath('file2.json'), fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8')],
+    ['stylish', getFixturePath('file1.yml'), getFixturePath('file2.yml'), fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8')],
+    ['plain', getFixturePath('file1.json'), getFixturePath('file2.yml'), fs.readFileSync(getFixturePath('resultPlain.txt'), 'utf-8')],
+    ['json', getFixturePath('file1.json'), getFixturePath('file2.json'), fs.readFileSync(getFixturePath('resultJSON.txt'), 'utf-8')],
+  ])('when file1 and file2 are compared using %s formatter returns what expected', (formatName, file1, file2, expectedFile) => {
+    expect(genDiff(file1, file2, formatName)).toBe(expectedFile);
+  });
 });
